df13b6b92254d96930cff8a062ab454a2ab3611f
Denis Kolodin <deniskolodin@gmail.com>
2020-03-07 00:34:24 +0900

    Use async Command for building

--- build.rs
+++ build.rs
@@ -1,32 +1,43 @@
+use async_trait::async_trait;
 use failure::{format_err, Error};
 use std::env;
-use std::io::{BufRead, BufReader, Read};
 use std::path::PathBuf;
-use std::process::{Command, Stdio};
+use std::process::Stdio;
+use tokio::io::{AsyncBufReadExt, AsyncRead, BufReader};
+use tokio::process::Command;
 
-trait RunIt {
-    fn run_it(&mut self, err: &str) -> Result<(), Error>;
+async fn print_all<T>(stream: Option<T>) -> Result<(), Error>
+where
+    T: AsyncRead + Unpin,
+{
+    if let Some(stream) = stream {
+        let mut lines = BufReader::new(stream).lines();
+        while let Some(line) = lines.next_line().await? {
+            eprint!("{}", line);
+        }
+    }
+    Ok(())
 }
 
+#[async_trait]
+trait RunIt {
+    async fn run_it(&mut self, err: &str) -> Result<(), Error>;
+}
+
+#[async_trait]
 impl RunIt for Command {
-    fn run_it(&mut self, err: &str) -> Result<(), Error> {
+    async fn run_it(&mut self, err: &str) -> Result<(), Error> {
+        self.env("RUST_LOG", "0");
         self.stdin(Stdio::null());
         self.stdout(Stdio::piped());
         self.stderr(Stdio::piped());
         let mut child = self.spawn()?;
-        // Important! We have to read stderr first, because wasm-pack required that.
-        if let Some(mut err) = child.stderr.take() {
-            let mut out = String::new();
-            err.read_to_string(&mut out)?;
-            eprintln!("{}", out);
-        }
-        if let Some(out) = child.stdout.take() {
-            let buf = BufReader::new(out);
-            for line in buf.lines() {
-                eprint!("{}", line?);
-            }
-        }
-        if !child.wait()?.success() {
+        let (_, _, res) = tokio::join!(
+            print_all(child.stdout.take()),
+            print_all(child.stderr.take()),
+            child,
+        );
+        if !res?.success() {
             Err(format_err!("{}", err))
         } else {
             Ok(())
@@ -34,16 +45,19 @@ impl RunIt for Command {
     }
 }
 
-fn main() -> Result<(), Error> {
+#[tokio::main]
+async fn main() -> Result<(), Error> {
     Command::new("wasm-pack")
         .args(&["build", "--target", "web"])
         .current_dir("ui")
-        .run_it("Can't compile UI")?;
+        .run_it("Can't compile UI")
+        .await?;
 
     Command::new("rollup")
         .args(&["./main.js", "--format", "iife", "--file", "./pkg/bundle.js"])
         .current_dir("ui")
-        .run_it("Can't rollup UI")?;
+        .run_it("Can't rollup UI")
+        .await?;
 
     let out_path = PathBuf::from(env::var("OUT_DIR")?);
     let tar_path = out_path.join("ui.tar.gz");
@@ -60,12 +74,14 @@ fn main() -> Result<(), Error> {
             "pkg/tody_chat_ui_bg.wasm",
         ])
         .current_dir("ui")
-        .run_it("Can't pack UI")?;
+        .run_it("Can't pack UI")
+        .await?;
 
     if cfg!(feature = "refresh") {
         Command::new("touch")
             .args(&["build.rs"])
-            .run_it("Can't touch the build file")?;
+            .run_it("Can't touch the build file")
+            .await?;
     }
     Ok(())
 }
