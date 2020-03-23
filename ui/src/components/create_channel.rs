015b931587f8730c2fa006d28ac56138ce2151f0
Denis Kolodin <deniskolodin@gmail.com>
2020-03-23 23:52:12 +0900

    Make channel_name updatable

--- ui/src/components/create_channel.rs
+++ ui/src/components/create_channel.rs
@@ -3,11 +3,13 @@ use yew::prelude::*;
 
 pub struct CreateChannel {
     link: ComponentLink<Self>,
+    channel_name: String,
     connector: Box<dyn Bridge<Connector>>,
 }
 
 pub enum Msg {
     FromConnector(Notification),
+    UpdateChannelName(String),
     CreateChannel,
 }
 
@@ -18,14 +20,23 @@ impl Component for CreateChannel {
     fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
         let callback = link.callback(|n| Msg::FromConnector(n));
         let connector = Connector::bridge(callback);
-        Self { link, connector }
+        Self {
+            link,
+            channel_name: String::new(),
+            connector,
+        }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
-            Msg::FromConnector(notification) => {}
+            Msg::FromConnector(notification) => {
+                log::debug!("Notification: {:?}", notification);
+            }
+            Msg::UpdateChannelName(channel_name) => {
+                self.channel_name = channel_name;
+            }
             Msg::CreateChannel => {
-                let action = Action::CreateChannel("test-channel".into());
+                let action = Action::CreateChannel(self.channel_name.clone());
                 self.connector.send(action);
             }
         }
@@ -36,7 +47,9 @@ impl Component for CreateChannel {
         html! {
             <div>
                 <label>{ "Channel name" }</label>
-                <input placeholder="Channel name" />
+                <input value=self.channel_name
+                       placeholder="Channel"
+                       oninput=self.link.callback(|e: InputData| Msg::UpdateChannelName(e.value)) />
                 <button onclick=self.link.callback(|_| Msg::CreateChannel)>{ "Create" }</button>
             </div>
         }
